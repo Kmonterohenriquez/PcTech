4307991e8b3fa64d62e8bada8855fa7aa4b9a3cb
Kevin Montero <kevinwilding1997@gmail.com>
2020-04-19 02:48:46 -0400

    Quote info structure done

--- src/Components/Quote/Quote.js
+++ src/Components/Quote/Quote.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from 'react';
 
 const Quote = () => {
-    return (
-        <div className='Quote'>
-            
-        </div>
-    )
-}
+	return (
+		<div className='Quote'>
+			<div className='left-side'>
+				<h3>Multi Brand</h3>
+				<h1>Sell & Repair</h1>
+				<p>
+					elit risus, aliquam id velit non, congue iaculis metus. Aliquam erat
+					volutpat. Nam tempus, dui a lobortis efficitur, purus orci pretium
+					nulla, sed posuere massa nulla id orci. Duis sit amet ante vel ante
+					rutrum mollis quis ac odio. Donec rutrum libero non sagittis venenatis
+				</p>
+			</div>
+			<div className='right-side'>
+                <form action="">
+                    <h1>Get a Free Quote</h1>
+                    <input type="name" placeholder='Your Name'/>
+                    <input type="email" placeholder='Your Email'/>
+                    <textarea name="" id="" cols="30" rows="10"  placeholder='Give Some Details'>
+                        
+                    </textarea>
+                    <button> Get a Quote</button>
+                </form>
+            </div>
+		</div>
+	);
+};
 
-export default Quote
+export default Quote;
