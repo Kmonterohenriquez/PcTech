136b0b01a0a92e819861e34f3589a0670cfb3ed3
Kevin Montero <kevinwilding1997@gmail.com>
2020-04-19 03:16:19 -0400

    Quote Styling component done

--- src/Components/Quote/Quote.js
+++ src/Components/Quote/Quote.js
@@ -3,7 +3,7 @@ import './Quote.sass'
 const Quote = () => {
 	return (
 		<div className='Quote'>
-			<div className='container'>
+			<div className='Quote-container sm-container'>
 				<div className='left-side'>
 					<h3>Multi Brand</h3>
 					<h1>Sell & Repair</h1>
@@ -12,6 +12,7 @@ const Quote = () => {
 						volutpat. Nam tempus, dui a lobortis efficitur, purus orci pretium
 						nulla, sed posuere massa nulla id orci. Duis sit amet ante vel ante
 						rutrum mollis quis ac odio. Donec rutrum libero non sagittis
+						venenatisrutrum mollis quis ac odio. Donec rutrum libero non sagittis
 						venenatis
 					</p>
 				</div>
@@ -27,7 +28,9 @@ const Quote = () => {
 							rows='10'
 							placeholder='Give Some Details'
 						></textarea>
-						<button> Get a Quote</button>
+						<div className='btn-container'>
+                            <button> Get a Quote</button>
+                        </div>
 					</form>
 				</div>
 			</div>
