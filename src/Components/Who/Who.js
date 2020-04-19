e63d9e0cc9d7e36c0067c5754eb0ec17401374e4
Kevin Montero <kevinwilding1997@gmail.com>
2020-04-19 02:01:06 -0400

    Who component info structure done

--- src/Components/Who/Who.js
+++ src/Components/Who/Who.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import img from '../../img/who.jpg'
 
 const Who = () => {
-    return (
-        <div className='Who'>
-            
-        </div>
-    )
-}
+	return (
+		<div className='Who'>
+			<div className='content'>
+				<h1>Who We Are </h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris elit
+					risus, aliquam id velit non, congue iaculis metus. Aliquam erat
+					volutpat. V
+				</p>
+				<button>View Services</button>
+			</div>
+            <img src={img} alt="Man repairing a computer."/>
+		</div>
+	);
+};
 
-export default Who
+export default Who;
