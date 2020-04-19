c061f2ec5a1d3031ed94dee93215becae0552de7
Kevin Montero <kevinwilding1997@gmail.com>
2020-04-19 03:58:33 -0400

    Info implemented into BottomLine Component

--- src/Components/BottomLine/BottomLine.js
+++ src/Components/BottomLine/BottomLine.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const BottomLine = () => {
-    return (
-        <div className='BottomLine'>
-            
-        </div>
-    )
-}
+	return (
+		<div className='BottomLine '>
+			<div className='container'>
+				<p>
+					© Copyright 2020. Design by{' '}
+					<a href='https://kevinmontero.com/'>Kevin Montero</a>
+				</p>
+				<div className='small-menu'>Home/ About Us / Contact Us</div>
+			</div>
+		</div>
+	);
+};
 
-export default BottomLine
+export default BottomLine;
