e1dd4d17e5cb41e4c906a4662d2ec510d0402ebb
Kevin Montero <kevinwilding1997@gmail.com>
2020-04-19 02:29:34 -0400

    Services info added

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,11 +1,15 @@
-import React from 'react'
-import './Services.sass'
+import React from 'react';
+import './Services.sass';
 const Services = () => {
-    return (
-        <div className='Services'>
-            
-        </div>
-    )
-}
+	return (
+		<div className='Services'>
+			<h1>Our Services</h1>
+			<p>
+				We are one of the best laptop repairing service provider company in New
+				York and repair your system at your home/office at very cheapest price
+			</p>
+		</div>
+	);
+};
 
-export default Services
+export default Services;
