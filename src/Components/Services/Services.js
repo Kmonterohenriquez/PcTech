e5c4cdf62ddd66f3397e67f35efba98c99a490e5
Kevin Montero <kevinwilding1997@gmail.com>
2020-04-19 02:39:09 -0400

    Services component done

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -3,11 +3,14 @@ import './Services.sass';
 const Services = () => {
 	return (
 		<div className='Services'>
-			<h1>Our Services</h1>
-			<p>
-				We are one of the best laptop repairing service provider company in New
-				York and repair your system at your home/office at very cheapest price
-			</p>
+			<div className='Services-container sm-container'>
+				<h1>Our Services</h1>
+				<p>
+					We are one of the best laptop repairing service provider company in
+					Orlando and repair your system at your home/office at very cheapest
+					price.
+				</p>
+			</div>
 		</div>
 	);
 };
