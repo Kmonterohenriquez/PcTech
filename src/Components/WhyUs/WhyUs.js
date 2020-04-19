734214aa998c8b973d59fa91edaa1eb5b72c4e7a
Kevin Montero <kevinwilding1997@gmail.com>
2020-04-19 00:48:11 -0400

    WhyUs component imported into Home component

--- src/Components/WhyUs/WhyUs.js
+++ src/Components/WhyUs/WhyUs.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from 'react';
+import './WhyUs.sass';
+import info from './info.js';
 
 const WhyUs = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+	return (
+		<div className='WhyUs'>
+			<h1>Why Choose Us</h1>
+			<p>Fastest repair service with best prices!</p>
 
-export default WhyUs
+			<div className='box'>
+				<div className='title'>Title here</div>
+				<div className='description'>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris elit
+					risus, aliquam id velit non, congue iaculis metus. Aliquam erat
+					volutpat.
+				</div>
+				<a>Read more</a>
+			</div>
+		</div>
+	);
+};
+
+export default WhyUs;
--- src/Page/Home/Home.js
+++ src/Page/Home/Home.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Showcase from '../../Components/Showcase/Showcase'
+import WhyUs from '../../Components/WhyUs/WhyUs'
+
 const Home = () => {
     return (
         <div className='Home'>
