53093ec872db8bdf548ea3f7dd41bd97c0fc37b9
Kevin Montero <kevinwilding1997@gmail.com>
2020-04-19 02:26:24 -0400

    Services structure component donen

---
+++ src/Components/Services/Services.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Services.sass'
+const Services = () => {
+    return (
+        <div className='Services'>
+            
+        </div>
+    )
+}
+
+export default Services
