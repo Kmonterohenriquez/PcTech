0ceeecccabdd525818ee68446475bb7f08a0213c
Kevin Montero <kevinwilding1997@gmail.com>
2020-04-19 00:29:00 -0400

    Showcase component done

--- src/Components/Showcase/Showcase.js
+++ src/Components/Showcase/Showcase.js
@@ -1,14 +1,27 @@
-import React from 'react'
-import bg from '../../img/Showcase.png'
-import './Showcase.sass'
+import React from 'react';
+import bg from '../../img/Showcase.png';
+import './Showcase.sass';
+import logo from '../../img/logo.png';
+import { Link } from 'react-router-dom';
 
 const Showcase = () => {
-    return (
-        <div className='Showcase'> 
-            <img src={bg} alt="Showcase Background"/>
-            <h1>asdasdasd</h1>
-        </div>
-    )
-}
+	return (
+		<div className='Showcase'>
+			<nav>
+				<ul>
+					<Link>Laptops</Link>
+					<Link>Desktop</Link>
+					<Link>Location</Link>
+					<Link>About Us</Link>
+				</ul>
+			</nav>
+			<img className='showcase-bg' src={bg} alt='Showcase Background' />
+            <div className="welcome-container">
+                <h1>Welcome to</h1>
+                <img className='logo' src={logo} alt="PC Tech logo"/>
+            </div>
+		</div>
+	);
+};
 
-export default Showcase
+export default Showcase;
