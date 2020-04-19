57ddc61b5100f3748001634438fbc54058ca585e
Kevin Montero <kevinwilding1997@gmail.com>
2020-04-19 03:49:44 -0400

    Footer styling component done

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,8 +5,8 @@ import './Footer.sass'
 const Footer = () => {
 	return (
 		<div className='Footer'>
-			<div className='container'>
-				<div className='side'>
+			<div className='Footer-container container'>
+				<div className='side-1'>
 					<img src={logo} alt='PC Tech' />
 					<p>
 						Being the best in our field means that we are committed to every
@@ -14,22 +14,26 @@ const Footer = () => {
 						every client happy.
 					</p>
 				</div>
-				<div className='side'>
+				<div className='side-2'>
 					<h1>Our services</h1>
 					<ul>
-						<li>Laptop Repair</li>
-						<li>Desktop Repair</li>
-						<li>Sell PC hardware</li>
-						<li>Sell Desktop</li>
-						<li>Sell Desktop</li>
+						<li><i className="fas fa-chevron-right"></i>Laptop Repair</li>
+                        <hr/>
+						<li><i className="fas fa-chevron-right"></i>Desktop Repair</li>
+                        <hr/>
+						<li><i className="fas fa-chevron-right"></i>Sell PC hardware</li>
+                        <hr/>
+						<li><i className="fas fa-chevron-right"></i>Sell Desktop</li>
+                        <hr/>
+						<li><i className="fas fa-chevron-right"></i>Sell Desktop</li>
 					</ul>
 				</div>
-				<div className='side'>
+				<div className='side-3'>
 					<h1>Contact Us</h1>
-					<p className='address'>123 Main Street Orlando, Fl. 9876</p>
-					<p className='phone'>(123) 456-7890</p>
-					<p className='fax'>(123) 456-7777</p>
-					<p className='email'>info@yourdomain.com</p>
+					<p className='address'><i className="fas fa-map-marker-alt"></i>123 Main Street Orlando, Fl. 9876</p>
+					<p className='phone'><i className="fas fa-phone-volume"></i>(123) 456-7890</p>
+					<p className='fax'><i className="fas fa-print"></i>(123) 456-7777</p>
+					<p className='email'><i className="far fa-envelope"></i>info@yourdomain.com</p>
 				</div>
 			</div>
 		</div>
