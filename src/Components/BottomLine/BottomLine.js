92645379812a943a63287b88274ca572f6359800
Kevin Montero <kevinwilding1997@gmail.com>
2020-04-19 04:03:45 -0400

    BottomLine styling done

--- src/Components/BottomLine/BottomLine.js
+++ src/Components/BottomLine/BottomLine.js
@@ -3,12 +3,12 @@ import './BottomLine.sass';
 const BottomLine = () => {
 	return (
 		<div className='BottomLine '>
-			<div className='container'>
+			<div className=' BottomLine-container container'>
 				<p>
 					© Copyright 2020. Design by{' '}
 					<a href='https://kevinmontero.com/'>Kevin Montero</a>
 				</p>
-				<div className='small-menu'>Home/ About Us / Contact Us</div>
+				<div className='small-menu'>Home / About Us / Contact Us</div>
 			</div>
 		</div>
 	);
