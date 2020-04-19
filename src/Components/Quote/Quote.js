b34f889c2303198dc5f62fe751ebce3403aa9136
Kevin Montero <kevinwilding1997@gmail.com>
2020-04-19 02:52:48 -0400

    Just started on Quote component  Styling

--- src/Components/Quote/Quote.js
+++ src/Components/Quote/Quote.js
@@ -1,29 +1,36 @@
 import React from 'react';
-
+import './Quote.sass'
 const Quote = () => {
 	return (
 		<div className='Quote'>
-			<div className='left-side'>
-				<h3>Multi Brand</h3>
-				<h1>Sell & Repair</h1>
-				<p>
-					elit risus, aliquam id velit non, congue iaculis metus. Aliquam erat
-					volutpat. Nam tempus, dui a lobortis efficitur, purus orci pretium
-					nulla, sed posuere massa nulla id orci. Duis sit amet ante vel ante
-					rutrum mollis quis ac odio. Donec rutrum libero non sagittis venenatis
-				</p>
+			<div className='container'>
+				<div className='left-side'>
+					<h3>Multi Brand</h3>
+					<h1>Sell & Repair</h1>
+					<p>
+						elit risus, aliquam id velit non, congue iaculis metus. Aliquam erat
+						volutpat. Nam tempus, dui a lobortis efficitur, purus orci pretium
+						nulla, sed posuere massa nulla id orci. Duis sit amet ante vel ante
+						rutrum mollis quis ac odio. Donec rutrum libero non sagittis
+						venenatis
+					</p>
+				</div>
+				<div className='right-side'>
+					<form action=''>
+						<h1>Get a Free Quote</h1>
+						<input type='name' placeholder='Your Name' />
+						<input type='email' placeholder='Your Email' />
+						<textarea
+							name=''
+							id=''
+							cols='30'
+							rows='10'
+							placeholder='Give Some Details'
+						></textarea>
+						<button> Get a Quote</button>
+					</form>
+				</div>
 			</div>
-			<div className='right-side'>
-                <form action="">
-                    <h1>Get a Free Quote</h1>
-                    <input type="name" placeholder='Your Name'/>
-                    <input type="email" placeholder='Your Email'/>
-                    <textarea name="" id="" cols="30" rows="10"  placeholder='Give Some Details'>
-                        
-                    </textarea>
-                    <button> Get a Quote</button>
-                </form>
-            </div>
 		</div>
 	);
 };
