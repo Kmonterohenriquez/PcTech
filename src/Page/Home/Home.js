5dffe82773f5e217864bb7c1262c988cb3b1f8ab
Kevin Montero <kevinwilding1997@gmail.com>
2020-04-19 02:02:04 -0400

    Who component imported into Home component

--- src/Page/Home/Home.js
+++ src/Page/Home/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Showcase from '../../Components/Showcase/Showcase';
 import WhyUs from '../../Components/WhyUs/WhyUs';
+import Who from '../../Components/Who/Who'
 
 const Home = () => {
-    
 	return (
 		<div className='Home'>
 			<Showcase />
 			<WhyUs />
+			<Who />
 		</div>
 	);
 };
