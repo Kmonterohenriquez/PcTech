fa62c7efbfeeb4b210b1f90b3a87b02a100dcddf
Kevin Montero <kevinwilding1997@gmail.com>
2020-04-21 19:09:11 -0400

    structure of Title component done

---
+++ src/Components/Title/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Title.sass'
+const Title = () => {
+    return (
+        <div className='Title'>
+            
+        </div>
+    )
+}
+
+export default Title
