73f7c84335584f81edec61ad084cea58a1a57a7a
Kevin Montero <kevinwilding1997@gmail.com>
2020-04-20 22:57:29 -0400

    AdminCenterNav structure component done

--- src/Components/AdminCenterNav/AdminCenterNav.js
+++ src/Components/AdminCenterNav/AdminCenterNav.js
@@ -3,7 +3,10 @@ import React from 'react'
 const AdminCenterNav = () => {
     return (
         <div className='AdminCenterNav'>
-            
+            <div className='nav'>
+                <i className="fas fa-chevron-left"></i>
+                <p>Client Side</p>
+            </div>
         </div>
     )
 }
