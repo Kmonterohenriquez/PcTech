3f9ffdfcf3579d7c89b437c16c169d8e61770b7b
Kevin Montero <kevinwilding1997@gmail.com>
2020-04-21 18:55:02 -0400

    Route of Laptops component created

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './Page/Home/Home';
 import AdminCenter from './Page/AdminCenter/AdminCenter';
-
+import Laptops from './Page/Laptops/Laptops';
 export default (
 	<Switch>
 		<Route component={Home} exact path='/' />
 		<Route component={AdminCenter} exact path='/admin_center' />
+		<Route component={Laptops} exact path='/laptops'/>
 	</Switch>
 );
