dab349afdf74b1754d39fa9463ca55c64396f1c0
Kevin Montero <kevinwilding1997@gmail.com>
2020-04-28 01:05:00 -0400

    Desktops component route created

--- src/Page/Desktops/Desktops.js
+++ src/Page/Desktops/Desktops.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Desktop.sass'
+import './Desktops.sass'
 const Desktop = () => {
     return (
         <div className='Desktop'>
--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './Page/Home/Home';
 import AdminCenter from './Page/AdminCenter/AdminCenter';
 import Laptops from './Page/Laptops/Laptops';
+import Desktops from './Page/Desktops/Desktops'
 export default (
 	<Switch>
 		<Route component={Home} exact path='/' />
 		<Route component={AdminCenter} exact path='/admin_center' />
 		<Route component={Laptops} exact path='/laptops'/>
+		<Route component={Desktops} exact path='/desktops'/>
 	</Switch>
 );
