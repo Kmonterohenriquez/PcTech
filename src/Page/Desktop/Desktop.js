c479b441cda34fc5d34c87e9c3517b3626db9fc1
Kevin Montero <kevinwilding1997@gmail.com>
2020-04-28 01:02:23 -0400

    Structure Desktop component done

---
+++ src/Page/Desktop/Desktop.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Desktop.sass'
+const Desktop = () => {
+    return (
+        <div className='Desktop'>
+            
+        </div>
+    )
+}
+
+export default Desktop
