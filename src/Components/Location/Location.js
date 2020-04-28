d0de4855d6b2c7711ed48027623678cc101dbf7c
Kevin Montero <kevinwilding1997@gmail.com>
2020-04-28 17:07:20 -0400

    Structure of Location component created

---
+++ src/Components/Location/Location.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Location.sass'
+
+const Location = () => {
+    return (
+        <div className='Location'>
+            
+        </div>
+    )
+}
+
+export default Location
