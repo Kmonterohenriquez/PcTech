770b148430d4bb3b42eb32e3e956968ac8e1636d
Kevin Montero <kevinwilding1997@gmail.com>
2020-05-01 16:50:56 -0400

    server running

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env;
-
+const app = express();
 app.use(express.json());
 const port = SERVER_PORT;
 app.listen(port, () => console.log(`Server running on port ${port}`));
