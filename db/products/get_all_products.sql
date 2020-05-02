98b08d6ab479cc69b66cf2dc6896ebd7111273d9
Kevin Montero <kevinwilding1997@gmail.com>
2020-05-02 00:06:17 -0400

    getAllProducts controller is done

---
+++ db/products/get_all_products.sql
@@ -0,0 +1,2 @@
+select * 
+from products_pctech;
--- server/controllers/productCtrl.js
+++ server/controllers/productCtrl.js
@@ -1,3 +1,9 @@
 module.exports = {
-    
+	getAllProducts: async () => {
+		const db = req.app.get('db');
+		await db.products
+			.get_all_products()
+            .then((products) => res.status(200).send(products))
+            .catch(err=> res.status(500).send(err));
+	},
 };
