8cf262f5547fa8490c1e15da3646ad49fbdbc4ae
Kevin Montero <kevinwilding1997@gmail.com>
2020-05-01 23:58:37 -0400

    database seed created

---
+++ db/seed.sql
@@ -0,0 +1,10 @@
+create table products_pctech (
+    product_id serial primary key,
+    pc_name varchar(250),
+    os varchar(100),
+    cpu varchar(250),
+    graphic_card varchar(250),
+    ram varchar(250),
+    motherboard varchar(250),
+    storage varchar(250)
+);
