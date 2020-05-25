d4b1bfb9c58b14ec51fe8d0f44f9b5dc81cdd7ad
Kevin Montero <kevinwilding1997@gmail.com>
2020-05-25 00:43:16 -0400

    static varialble on server implemented

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const express = require('express'),
 	app = express();
 
 app.use(express.json());
-
+app.use(express.static(`${__dirname}/../build`));
 // MASSIVE -v 3.1.4
 massive(CONNECTION_STRING)
 	.then((db) => {
