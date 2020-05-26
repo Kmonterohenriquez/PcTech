550474ffcf58c6a5225d5aa0ec3056bee7236c91
Kevin Montero <kevinwilding1997@gmail.com>
2020-05-26 14:17:00 -0400

    social media icons added and paragraph color changed on Footer component

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from '../../img/logo.png';
-import './Footer.sass'
+import './Footer.sass';
 
 const Footer = () => {
 	return (
@@ -13,27 +13,58 @@ const Footer = () => {
 						project, we have ingenious ideas that become reality and we make
 						every client happy.
 					</p>
+					<div className='social-media'>
+						<i className='fab fa-facebook-f'></i>
+						<p>/</p>
+						<i className='fab fa-twitter'></i>
+						<p>/</p>
+						<i className='fab fa-youtube'></i>
+						<p>/</p>
+						<i className='fab fa-linkedin-in'></i>
+						<p>/</p>
+						<i className='fab fa-instagram'></i>
+						<p>/</p>
+					</div>
 				</div>
 				<div className='side-2'>
 					<h1>Our services</h1>
 					<ul>
-						<li><i className="fas fa-chevron-right"></i>Laptop Repair</li>
-                        <hr/>
-						<li><i className="fas fa-chevron-right"></i>Desktop Repair</li>
-                        <hr/>
-						<li><i className="fas fa-chevron-right"></i>Sell PC hardware</li>
-                        <hr/>
-						<li><i className="fas fa-chevron-right"></i>Sell Desktop</li>
-                        <hr/>
-						<li><i className="fas fa-chevron-right"></i>Sell Desktop</li>
+						<li>
+							<i className='fas fa-chevron-right'></i>Laptop Repair
+						</li>
+						<hr />
+						<li>
+							<i className='fas fa-chevron-right'></i>Desktop Repair
+						</li>
+						<hr />
+						<li>
+							<i className='fas fa-chevron-right'></i>Sell PC hardware
+						</li>
+						<hr />
+						<li>
+							<i className='fas fa-chevron-right'></i>Sell Desktop
+						</li>
+						<hr />
+						<li>
+							<i className='fas fa-chevron-right'></i>Sell Desktop
+						</li>
 					</ul>
 				</div>
 				<div className='side-3'>
 					<h1>Contact Us</h1>
-					<p className='address'><i className="fas fa-map-marker-alt"></i>123 Main Street Orlando, Fl. 9876</p>
-					<p className='phone'><i className="fas fa-phone-volume"></i>(123) 456-7890</p>
-					<p className='fax'><i className="fas fa-print"></i>(123) 456-7777</p>
-					<p className='email'><i className="far fa-envelope"></i>info@yourdomain.com</p>
+					<p className='address'>
+						<i className='fas fa-map-marker-alt'></i>123 Main Street Orlando,
+						Fl. 9876
+					</p>
+					<p className='phone'>
+						<i className='fas fa-phone-volume'></i>(123) 456-7890
+					</p>
+					<p className='fax'>
+						<i className='fas fa-print'></i>(123) 456-7777
+					</p>
+					<p className='email'>
+						<i className='far fa-envelope'></i>info@yourdomain.com
+					</p>
 				</div>
 			</div>
 		</div>
