3f384dda3ad4a90ab27adc2fae8acdfd24d1293f
Kevin Montero <kevinwilding1997@gmail.com>
2020-05-27 02:51:01 -0400

    Structure of Counter component done

---
+++ src/Components/Counter/Counter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Counter.sass'
+const Counter = () => {
+    return (
+        <div className='Counter'>
+            
+        </div>
+    )
+}
+
+export default Counter
