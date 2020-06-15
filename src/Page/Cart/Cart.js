cc89b4e558ead55d8583ccfb856d39ca33ee7264
Kevin Montero <kevinwilding1997@gmail.com>
2020-06-15 00:38:53 -0400

    ItemOnCart component created

---
+++ src/Page/Cart/Cart.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Cart.sass'
+
+const Cart = () => {
+    return (
+        <div className='Cart'>
+            <div className='header'>
+                <img src="" alt="" className="logo"/>
+                <h1>Your Shopping Cart</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Cart
