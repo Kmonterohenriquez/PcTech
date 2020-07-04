128f570ba0817c3707ea1fddfdf2a206bb50c406
Kevin Montero <kevinwilding1997@gmail.com>
2020-07-03 23:30:04 -0400

    Checkout page created

---
+++ src/Page/Checkout/Checkout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Checkout.sass'
+
+const Checkout = () => {
+    return (
+        <div className='Checkout'>
+            
+        </div>
+    )
+}
+
+export default Checkout
